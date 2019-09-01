c8b5a028966315ac3f6b9a8f14a6dc9a63714bc6
Zheng Fang <ustc.zheng@gmail.com>
2019-09-01 12:31:28 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ is assumed to have the following:
 	1) The dynamical system. If calling one of the built-in examples, the name
 	of the dynamics must have a match in 'lib_dynamics.py'; if builing from 
 	scratch, 'def_dynamics.py' must be ready at this point.
-	2) The data. If performing twin-experiments, the specs should be given 
-	below; if working with real data, the data should be prepared according to
-	the user manual.
+	2) The data. If performing twin-experiments, the specs should be given but 
+	a data file is not required; if working with real data, the data should be 
+	prepared according to the user manual.
 	3) If external stimuli are needed, a .npy file containing the time series; 
 	4) Configuration of the code, including the hyper-parameters for PAHMC. 
 	Refer to the manual for the shape and type requirements. Also note that a 
@@ -18,7 +18,7 @@ is assumed to have the following:
 	manual for details.
 
 It is suggested that the user keep a lookup table for the model paramters to 
-make it easier to preserve order when working on the above three steps.
+make it easier to preserve order when working on the above steps.
 """
 
 
