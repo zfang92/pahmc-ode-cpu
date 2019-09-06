c5127b013664d12932b4731b1f5e53a763eee96a
Zheng Fang <ustc.zheng@gmail.com>
2019-09-06 15:16:43 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ make it easier to preserve order when working on the above steps.
 """
 
 
+from datetime import date
+from pathlib import Path
+
 import numpy as np
 import time
 
@@ -34,17 +37,17 @@ from pahmc_ode_cpu import lib_dynamics
 #=========================type your code below=========================
 """A name for your dynamics."""
 # it will be used to try to find a match in the built-ins
-name = 'lorenz96'
+name = 'nakl'
 
 """Specs for the dynamics."""
 # set the dimension of your dynamics
-D = 20
+D = 4
 # set the length of the observation window
-M = 200
+M = 5000
 # set the observed dimensions (list with smallest possible value 1)
-obsdim = [1, 2, 4, 6, 8, 10, 12, 14, 16, 17, 19, 20]
+obsdim = [1]
 # set the discretization interval
-dt = 0.025
+dt = 0.02
 
 """Specs for precision annealing and HMC."""
 # set the starting Rf value
@@ -64,20 +67,27 @@ mass = (1e0, 1e0, 1e0)
 # set the HMC scaling parameter for each beta
 scaling = 1.0
 # set the "soft" dynamical range for initialization purpose
-soft_dynrange = (-10, 10)
+soft_dynrange = [(-120, 0), (0, 1), (0, 1), (0, 1)]
 # set an initial guess for the parameters
-par_start = 8.0
+par_start = np.array([0.8,
+                      100, 80, 30, -60, 0.5, -55, 
+                      -50, 20, 0.2, 0.5,
+                      -50, -18, 2, 10,
+                      -60, 35, 2, 8])
 
 """Sepcs for the twin-experiment data"""
 # set the length of the data (must be greater than M defined above)
-length = 1000
-# set the noise level (standard deviation) in the data
-noise = 0.4
+length = int(1000/dt)
+# set the noise levels (standard deviations) in the data for each dimension
+noise = np.array([1, 0, 0, 0])
 # set the true parameters (caution: order must be consistent)
-par_true = 8.17
+par_true = np.array([1, 
+                     120, 50, 20, -77, 0.3, -54.4, 
+                     -40, 15, 0.1, 0.4, 
+                     -60, -15, 1, 7, 
+                     -55, 30, 1, 5])
 # set the initial condition for the data generation process
-x0 = np.ones(D)
-x0[0] = 0.01
+x0 = np.array([-70, 0.1, 0.9, 0.1])
 #===============================end here===============================
 
 
@@ -130,4 +140,32 @@ Rf, Rm, X_init, X_mean, Xfinal_history \
            epsilon, S, mass, scaling, soft_dynrange, par_start)
 
 print(f'Total time = {time.perf_counter()-t0:.2f} seconds.')
+
+
+"""Save the results."""
+day = date.today().strftime('%Y-%m-%d')
+i = 1
+while (Path.cwd() / 'user_results' / f'{name}_{day}_{i}.npz').exists():
+    i = i + 1
+
+np.savez(Path.cwd()/'user_results'/f'{name}_{day}_{i}', 
+         name=name, 
+         D=D, M=M, obsdim=obsdim, dt=dt, 
+         Rf0=Rf0, alpha=alpha, betamax=betamax, 
+         n_iter=n_iter, epsilon=epsilon, S=S, mass=mass, scaling=scaling, 
+         soft_dynrange=soft_dynrange, par_start=par_start, 
+         length=length, noise=noise, par_true=par_true, x0=x0, 
+         acceptance=acceptance, 
+         action=action, 
+         action_meanpath=action_meanpath, 
+         burn=burn, 
+         FE_meanpath=FE_meanpath, 
+         ME_meanpath=ME_meanpath, 
+         par_history=par_history, 
+         par_mean=par_mean, 
+         Rf=Rf, 
+         Rm=Rm, 
+         X_init=X_init, 
+         X_mean=X_mean, 
+         Xfinal_history=Xfinal_history)
 
