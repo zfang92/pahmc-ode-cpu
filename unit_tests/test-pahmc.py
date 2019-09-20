721b26da908b340065f972a0bf68d22beaf97360
Zheng Fang <ustc.zheng@gmail.com>
2019-09-19 23:52:10 -0700

    Add files via upload

--- unit_tests/test-pahmc.py
+++ unit_tests/test-pahmc.py
@@ -10,8 +10,7 @@ To run this unit test, copy this file into its parent directory and run it.
 """
 
 
-from datetime import date
-from pathlib import Path
+import cProfile, pstats
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,59 +25,52 @@ from pahmc_ode_cpu import lib_dynamics
 #=========================type your code below=========================
 """A name for your dynamics."""
 # it will be used to try to find a match in the built-ins
-name = 'nakl'
+name = 'lorenz96'
 
 """Specs for the dynamics."""
 # set the dimension of your dynamics
-D = 4
+D = 20
 # set the length of the observation window
-M = 5000
+M = 200
 # set the observed dimensions (list with smallest possible value 1)
-obsdim = [1]
+obsdim = [1, 2, 4, 6, 8, 10, 12, 14, 15, 17, 19, 20]
 # set the discretization interval
-dt = 0.02
+dt = 0.025
 
 """Specs for precision annealing and HMC."""
 # set the starting Rf value
-Rf0 = 0.1
+Rf0 = 1e6
 # set alpha
-alpha = 1.7
+alpha = 1.0
 # set the total number of beta values
-betamax = 30
+betamax = 1
 # set the number of HMC samples for each beta
 n_iter = 1000
 # set the HMC simulation stepsize for each beta
-epsilon = 1e-5
+epsilon = 1e-2
 # set the number of leapfrog steps for an HMC sample for each beta
-S = 100
+S = 150
 # set the HMC masses for each beta
 mass = (1e0, 1e0, 1e0)
 # set the HMC scaling parameter for each beta
-scaling = 1e6
+scaling = 1.0
 # set the "soft" dynamical range for initialization purpose
-soft_dynrange = [(-120, 0), (0, 1), (0, 1), (0, 1)]
+soft_dynrange = (-10, 10)
 # set an initial guess for the parameters
-par_start = (0.8,
-             100, 80, 30, -60, 0.5, -55, 
-             -50, 20, 0.2, 0.5,
-             -50, -18, 2, 10,
-             -60, 35, 2, 8)
+par_start = 8.0
 
-"""Sepcs for the twin-experiment data"""
+"""Specs for the twin-experiment data"""
 # set the length of the data (must be greater than M defined above)
-length = int(1000/dt)
+length = 1000
 # set the noise levels (standard deviations) in the data for each dimension
-noise = np.array([1, 0, 0, 0])
+noise = 0.4 * np.ones(D)
 # set the true parameters (caution: order must be consistent)
-par_true = (1, 
-            120, 50, 20, -77, 0.3, -54.4, 
-            -40, 15, 0.1, 0.4, 
-            -60, -15, 1, 7, 
-            -55, 30, 1, 5)
+par_true = 8.17
 # set the initial condition for the data generation process
-x0 = np.array([-70, 0.1, 0.9, 0.1])
+x0 = np.ones(D)
+x0[0] = 0.01
 # set the switch for discarding the first half of the generated data
-burndata = False
+burndata = True
 #===============================end here===============================
 
 
@@ -125,54 +117,29 @@ t0 = time.perf_counter()
 
 job = Core(dyn, Y, dt, D, obsdim, M)
 
-# cProfile.run('acceptance, action, action_meanpath, burn, ' \
-#              + 'FE_meanpath, ME_meanpath, par_history, par_mean, ' \
-#              + 'Rf, Rm, X_init, X_mean, Xfinal_history ' \
-#              + '= job.pa(Rf0, alpha, betamax, n_iter, ' \
-#              + 'epsilon, S, mass, scaling, soft_dynrange, par_start)')
-acceptance, action, action_meanpath, burn, \
-FE_meanpath, ME_meanpath, par_history, par_mean, \
-Rf, Rm, X_init, X_mean, Xfinal_history \
-  = job.pa(Rf0, alpha, betamax, n_iter,
-           epsilon, S, mass, scaling, soft_dynrange, par_start)
+cProfile.run('acceptance, action, action_meanpath, burn, ' \
+             + 'FE_meanpath, ME_meanpath, par_history, par_mean, ' \
+             + 'Rf, Rm, X_init, X_mean, Xfinal_history ' \
+             + '= job.pa(Rf0, alpha, betamax, n_iter, ' \
+             + 'epsilon, S, mass, scaling, soft_dynrange, par_start)', 
+             'profiling')
+# acceptance, action, action_meanpath, burn, \
+# FE_meanpath, ME_meanpath, par_history, par_mean, \
+# Rf, Rm, X_init, X_mean, Xfinal_history \
+#   = job.pa(Rf0, alpha, betamax, n_iter,
+#            epsilon, S, mass, scaling, soft_dynrange, par_start)
 
 print(f'Total time = {time.perf_counter()-t0:.2f} seconds.')
 
-
-# # plot action vs. iteration
-# fig, ax = plt.subplots(figsize=(6,5))
-# textblue = (49/255, 99/255, 206/255)
-# ax.loglog(action[0, :], color=textblue, lw=1.5)
-# ax.set_xlim(1, n_iter+1)
-# ax.set_xlabel('iteration')
-# ax.set_ylabel('action')
+p = pstats.Stats('profiling')
+p.strip_dirs().sort_stats(pstats.SortKey.CUMULATIVE).print_stats(10)
 
 
-# save variables
-day = date.today().strftime('%Y-%m-%d')
-i = 1
-while (Path.cwd() / 'user_results' / f'{name}_{day}_{i}.npz').exists():
-    i = i + 1
-
-np.savez(Path.cwd()/'user_results'/f'{name}_{day}_{i}', 
-         name=name, 
-         D=D, M=M, obsdim=obsdim, dt=dt, 
-         Rf0=Rf0, alpha=alpha, betamax=betamax, 
-         n_iter=n_iter, epsilon=epsilon, S=S, mass=mass, scaling=scaling, 
-         soft_dynrange=soft_dynrange, par_start=par_start, 
-         length=length, 
-         noise=noise, par_true=par_true, x0=x0, burndata=burndata, 
-         acceptance=acceptance, 
-         action=action, 
-         action_meanpath=action_meanpath, 
-         burn=burn, 
-         FE_meanpath=FE_meanpath, 
-         ME_meanpath=ME_meanpath, 
-         par_history=par_history, 
-         par_mean=par_mean, 
-         Rf=Rf, 
-         Rm=Rm, 
-         X_init=X_init, 
-         X_mean=X_mean, 
-         Xfinal_history=Xfinal_history)
+# plot action vs. iteration
+fig, ax = plt.subplots(figsize=(6,5))
+textblue = (49/255, 99/255, 206/255)
+ax.loglog(action[0, :], color=textblue, lw=1.5)
+ax.set_xlim(1, n_iter+1)
+ax.set_xlabel('iteration')
+ax.set_ylabel('action')
 
